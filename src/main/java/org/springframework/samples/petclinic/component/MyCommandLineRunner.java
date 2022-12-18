fb3a7e4d48c500b0994ae9205155148d99b16a5d
Volker Simonis <volker.simonis@gmail.com>
2022-12-18 21:02:32 +0100

    Added test data (first, last, street and city names) as Json files

--- src/main/java/org/springframework/samples/petclinic/component/MyCommandLineRunner.java
+++ src/main/java/org/springframework/samples/petclinic/component/MyCommandLineRunner.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Component
 public class MyCommandLineRunner implements CommandLineRunner {
 
@@ -14,7 +16,20 @@ public class MyCommandLineRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("=> MyCommandLineRunner started with: {}", Arrays.toString(args));
+		logger.info("=> MyCommandLineRunner: started with {}", Arrays.toString(args));
+		ObjectMapper mapper = new ObjectMapper();
+		String[] firstNames = mapper.readValue(
+				MyCommandLineRunner.class.getClassLoader().getResourceAsStream("data/firstNames.json"), String[].class);
+		logger.info("=> MyCommandLineRunner: read {} first names", firstNames.length);
+		String[] lastNames = mapper.readValue(
+				MyCommandLineRunner.class.getClassLoader().getResourceAsStream("data/lastNames.json"), String[].class);
+		logger.info("=> MyCommandLineRunner: read {} last names", lastNames.length);
+		String[] streetNames = mapper.readValue(
+				MyCommandLineRunner.class.getClassLoader().getResourceAsStream("data/streets.json"), String[].class);
+		logger.info("=> MyCommandLineRunner read {} street names", streetNames.length);
+		String[] cityNames = mapper.readValue(
+				MyCommandLineRunner.class.getClassLoader().getResourceAsStream("data/cities.json"), String[].class);
+		logger.info("=> MyCommandLineRunner read {} city names", cityNames.length);
 	}
 
 }
